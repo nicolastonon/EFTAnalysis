109055427fe644a9e49acdec89b147ddfba824a0
ntonon <nicolas.tonon@cern.ch>
2020-10-25 14:34:36 +0100

    update

--- Push_to_Git.sh
+++ Push_to_Git.sh
@@ -3,6 +3,6 @@ git add .
 git commit -m "update"
 
 #Dont know why, but git always want to push this repo even if I tell it not to track it...
-git rm -r --cached ./weightsMVA/
+# git rm -r --cached ./weightsMVA/
 
 git push origin master
